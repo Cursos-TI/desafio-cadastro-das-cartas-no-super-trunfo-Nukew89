4112a0351d37592586ab2483b6d1324d49955fe7
HSGamer <58573067+Nukew89@users.noreply.github.com>
2025-03-04 23:37:05 +0000

    Pt1 saida de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,11 +18,35 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    char estado[50];
+    int populacao;
+    float area;
+    int pontoTuristico;
+   
 
-        int populacao;
-        float area;
-        float PIB;
-        int pontosturisticos;
+    printf("Cadastre os dados das duas cartas do Super Trunfo: \n");
 
+    printf("Qual nome do Estado?: \n");
+    scanf("%s", estado);
+
+    printf("Qual a Populacao desse estado? \n");
+    scanf("%d", &populacao);
+
+    printf("Qual Área aproximada desse estado? \n");
+    scanf("%f", &area);
+
+    printf("Quantos Pontos turisticos Possue este Estado? \n");
+    scanf("%d", &pontoTuristico);
+
+    printf("Dados da carta 1 sao: \n");
+
+    printf("Nome do estado: %s \n", estado);
+    printf("Com uma populacao de: %d \n", populacao);
+    printf("Sua area aproximada é de: %f \n", area);
+    printf("Quantidade de Pontos Turisticos: %d \n", pontoTuristico);
+        
+
+    
+ 
     return 0;
 }
