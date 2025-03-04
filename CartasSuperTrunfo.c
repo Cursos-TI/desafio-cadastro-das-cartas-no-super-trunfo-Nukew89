73269de1d38b7a2e2cd3a292bda2e97a8823b3fb
HSGamer <58573067+Nukew89@users.noreply.github.com>
2025-03-04 23:07:54 +0000

    Variavel inicial

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,11 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+
+        int populacao;
+        float area;
+        float PIB;
+        int pontosturisticos;
+
     return 0;
 }
